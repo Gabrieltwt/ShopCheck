e2665b44dbc2f7e6e2e367f6a1de7dc4a0ca3c5f
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-10-02 19:57:54 -0300

    init commit

---
+++ src/test/java/dev/wakandaacademy/ShopCheck/ShopCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.ShopCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
